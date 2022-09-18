0aac50b06ba3171541bd359549e5db3d86d49147
G1g4lo <3163083993@qq.com>
2022-09-18 22:14:46 +0800

    x

--- src/testres/test_crypto.rs
+++ src/testres/test_crypto.rs
@@ -1,6 +1,6 @@
-
+#![allow(unused)]
 #[test]
-pub fn test_numToChar() {
+pub fn test_num_to_char() {
     let key:Vec<u8> = vec![148, 38, 92, 83];
     let key_string = base64::encode(&key);
     println!("{}", base64::encode(&key));
